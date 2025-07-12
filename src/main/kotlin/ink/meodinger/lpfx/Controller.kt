be691186b65b1c95ab01267099447b0cc71d0029
zhongsheng <371960474@qq.com>
2025-07-12 15:36:41 +0800

    支持command+左键点击，多选

--- src/main/kotlin/ink/meodinger/lpfx/Controller.kt
+++ src/main/kotlin/ink/meodinger/lpfx/Controller.kt
@@ -382,8 +382,10 @@ class Controller(private val state: State) {
         cLabelPane.setOnLabelClick  handler@{
             when (state.workMode) {
                 WorkMode.InputMode -> {
+                    // Check if Command/Meta key is pressed for multi-selection
+                    val shouldClear = !it.sourceEvent.isMetaDown
                     // Update selection
-                    cTreeView.selectLabel(it.labelIndex, clear = true, scrollTo = true)
+                    cTreeView.selectLabel(it.labelIndex, clear = shouldClear, scrollTo = true)
                     // Move to center if double-click
                     if (it.sourceEvent.isDoubleClick) cLabelPane.moveToLabel(it.labelIndex)
                 }
